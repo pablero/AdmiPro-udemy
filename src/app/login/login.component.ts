43710c8c7ccb320bcfdd687663c7e5875bb82b3c
Pablo <pabloalbanese15@gmail.com>
2020-07-10 19:32:59 -0300

    Actualización sección 7

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare function init_plugins();
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,6 +15,7 @@ export class LoginComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    init_plugins();
   }
 
   iniciar(){
